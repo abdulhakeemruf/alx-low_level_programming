fd9fe0c5947f5b1e88e552b112db58f7bf9a31ba
root <abdulhakeemrufai3@gmail.com>
2023-08-10 19:09:13 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+ * main - A prgram to print exactly
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-19-19\n",
+59);
+return (1);
+}
